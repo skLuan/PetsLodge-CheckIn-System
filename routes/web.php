5c5fed558b297b91cd0225a6f34a3179560fef05
skLuan <luan.erazo@skyrocket.com.co>
2025-07-07 16:25:15 -0500

    first commit added with the base of the checkin

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::redirect('/', '/check-in');
+Route::redirect('/checkin', '/check-in');
+Route::get('/check-in', function () {
+    return view('checkin');
+})->name('checkin');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
