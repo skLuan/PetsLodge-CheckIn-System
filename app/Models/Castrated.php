8601e9137281c865f06d581122b027974f7ab614
skLuan <luan.erazo@skyrocket.com.co>
2025-07-14 12:57:11 -0500

    added the castrated missing table

---
+++ app/Models/Castrated.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Castrated extends Model
+{
+    use HasFactory;
+}
