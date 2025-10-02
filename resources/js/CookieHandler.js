8072f62e942f4e0c04632029954c3f9527329522
skLuan <luan.erazo@skyrocket.com.co>
2025-10-01 20:08:09 -0500

    init refactoring data handler

--- resources/js/CookieHandler.js
+++ resources/js/CookieHandler.js
@@ -1,19 +1,91 @@
+import { info } from "autoprefixer";
 import Utils from "./Utils";
+
+const user = {
+    info: {
+        phone: "1234567890",
+        name: "Jhon Doe",
+        email: "jhon.doe@example.com",
+        address: "fake street #123",
+        city: "Cali",
+        zip: "760026",
+    },
+    emergencyContact: { name: "Jane Doe", phone: "123-456-7890" },
+};
+const pet = {
+    info: {
+        petName: "Max",
+        petColor: "Black",
+        petType: "dog",
+        petBreed: "Aleman",
+        petAge: "2022-01-09",
+        petWeight: "40",
+        petGender: "male",
+        petSpayed: "yes",
+    },
+    health: {
+        unusualHealthBehavior: false,
+        healthBehaviors: "Diarrhea",
+        warnings: "Angry with strangers",
+    },
+    feeding: [
+        {
+            day_time: "morning",
+            //type: "food",
+            feeding_med_details: "Water",
+        },
+        {
+            day_time: "Noon",
+            //type: "food",
+            feeding_med_details: "Water",
+        },
+    ],
+    medication: [
+        {
+            med_name: "Antibiotic",
+            med_dosage: "500mg",
+            med_frequency: "Twice a day",
+        },
+        {
+            med_name: "Painkiller",
+            med_dosage: "250mg",
+            med_frequency: "Once a day",
+        },
+    ],
+};
+
+const pets = [pet];
+const checkin = {
+    date: "",
+    user: user,
+    pets: pets,
+    grooming: { bath: false, nails: false, grooming: false },
+    groomingDetails: "",
+    inventory: [
+        "Leash",
+    ]
+};
 class CookieHandler {
     static setCookie(name, value, days = 1) {
         const expires = new Date(Date.now() + days * 864e5).toUTCString();
-        document.cookie = name + '=' + encodeURIComponent(JSON.stringify(value)) + '; expires=' + expires + '; path=/';
+        document.cookie =
+            name +
+            "=" +
+            encodeURIComponent(JSON.stringify(value)) +
+            "; expires=" +
+            expires +
+            "; path=/";
     }
 
     static getCookie(name) {
-        return document.cookie.split('; ').reduce((r, v) => {
-            const parts = v.split('=');
-            return parts[0] === name ? decodeURIComponent(parts[1]) : r
-        }, '');
+        return document.cookie.split("; ").reduce((r, v) => {
+            const parts = v.split("=");
+            return parts[0] === name ? decodeURIComponent(parts[1]) : r;
+        }, "");
     }
 
     static deleteCookie(name) {
-        this.setCookie(name, '', -1);
+        this.setCookie(name, "", -1);
     }
 
     // Function to get form data from cookies
@@ -25,7 +97,9 @@ class CookieHandler {
         console.log("Getting form data from cookies...");
         if (!cookie) return null;
         try {
-            return JSON.parse(decodeURIComponent(cookie.split("=").slice(1).join("=")));
+            return JSON.parse(
+                decodeURIComponent(cookie.split("=").slice(1).join("="))
+            );
         } catch (e) {
             console.error("Error parsing cookie JSON:", e);
             return null;
@@ -47,8 +121,11 @@ class CookieHandler {
         let existingData = this.getFormDataFromCookies(step);
 
         if (existingData[`pet${petIndex}`]) {
-            existingData[`pet${petIndex}`] = { ...existingData[`pet${petIndex}`], ...data };
-            
+            existingData[`pet${petIndex}`] = {
+                ...existingData[`pet${petIndex}`],
+                ...data,
+            };
+
             CookieHandler.setCookie(cookieKey, existingData);
             console.log(`Pet at index ${petIndex} updated in cookies.`);
         } else {
